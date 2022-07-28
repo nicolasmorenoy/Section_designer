cdd9f22b6b607e4dc76e38f06d8d9949b5b9aa60
unknown <nicolasmorenoy@gmail.com>
2022-07-28 17:25:30 -0500

    'work_on_classes_script'

--- classes.py
+++ classes.py
@@ -1,4 +1,30 @@
 
+from turtle import width
+
+
+class RectangularGeometry:
+    """
+    Class with all the geometry asspects of the element, including lenght of span; base, height
+    """
+
+    def __init__(self, width, height, lenght):
+        self.widht = width
+        self.height = height
+        self.lenght = lenght
+
+
+class Materials:
+    """
+    Class with all the information about the materials to be used in the element.
+    """
+
+    def __init__(self, fc, fy, EC, ES):
+        self.fc = fc
+        self.fy = fy
+        self.EC = EC
+        self.ES = ES
+
+
 class Beam:
     """
     Class that contains all the information about a created beam
