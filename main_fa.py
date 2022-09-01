7549d8ce72015410c6672b067f7904dcd994f0c8
unknown <nicolasmorenoy@gmail.com>
2022-09-01 00:32:26 -0500

    Geometry_post_model

--- main_fa.py
+++ main_fa.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+
+#Models
+
+class BeamGeometry(BaseModel):
+    width: float
+    height: float
+    lenght: Optional[float] = 5.0
+    cover: float
+
+
+#Path Operations
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+@app.post("/beam/geometry")
+def geometry(geometry: BeamGeometry = Body(...)):
+    return geometry
