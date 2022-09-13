a5b6818b31976df75211b3f875de40855dd992c4
unknown <nicolasmorenoy@gmail.com>
2022-09-13 11:45:39 -0500

    validations models

--- main_fa.py
+++ main_fa.py
@@ -1,8 +1,10 @@
 #Python
+from enum import Enum
 from typing import Optional
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI, Body, Query, Path
@@ -14,14 +16,40 @@ app = FastAPI()
 
 #Models
 
+class Sections(Enum):
+    rectangular: str = "rectangular"
+    circular: str =  "circular"
+
 class BeamGeometry(BaseModel):
-    width: float = 0.3
-    height: float = 0.4
-    lenght: Optional[float] = 5.0
+    width: float = Field(
+        ...,
+        gt = 0,
+        lt= 2,
+        example = 0.3
+    )
+    height: float = Field(
+        ...,
+        gt = 0,
+        lt= 2,
+        example = 0.4
+    )
+    lenght: Optional[float] = Field(
+        ...,
+        gt = 0,
+        lt= 2,
+        example = 0.3
+    )
+    section: Sections = Field(
+        ...,
+        example = "rectangular")
 
 
 class Beam(BeamGeometry):
-    beam_id: int
+    beam_id: int = Field(
+        ...,
+        gt = 0,
+        example = 1
+    )
 
 
 #Path Operations
@@ -31,17 +59,22 @@ def home():
     return {"Hello": "World"}
 
 
+##Request Body
 @app.post("/beam/geometry")
-def geometry(geometry: Beam = Body(...)):
+def beam(
+    geometry: Beam = Body(...)
+):
     width: float = geometry.width
     height: float = geometry.height
     length: float = geometry.lenght
-    beam_id: int = geometry.beam_id
-    section = Rectangular(width, height, length)
-    section_dict = {"cross area": section.area_1_2}
+    section: str = geometry.section.value
+    if section == "rectangular":
+        beam = Rectangular(width, height, length)
+    section_dict = {"cross area": beam.area_1_2}
     return section_dict
 
 
+##Query Parameters
 @app.get("/beam")
 def show_geometry_properties(
     width: float = Query(
@@ -69,6 +102,8 @@ def show_geometry_properties(
     geometry = Rectangular(width, height, lenght)
     return {"cross area": geometry.area_1_2}
 
+
+##Path Parameters
 @app.get("/beam/{beam_id}")
 def show_beam(
     beam_id: int = Path(
