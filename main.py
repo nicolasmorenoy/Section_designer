ff0fe587b1cf075f4055fb0e6704c5c2a154a50e
unknown <nicolasmorenoy@gmail.com>
2022-09-20 18:19:36 -0500

    documentation

--- main.py
+++ main.py
@@ -11,5 +11,8 @@ beam = Beam(geometry, 5, 0.04, concrete,steel, top_rebar, bottom_rebar, stirrup_
 
 
 if __name__ == '__main__':
-    print (beam.simple_bottom_nominal_moment_strenght)
+    print (beam.simple_top_nominal_moment_strenght)
+    top_rebar= ReinforcementProperties(2,Reinforcement(6))
+    print (beam.simple_top_nominal_moment_strenght)
+
     #print(beam.rebar.top_rebar_diameter, beam.rebar.top_rebar_area*10000, beam.top_effective_height, beam.simple_top_nominal_moment_strenght, beam.simple_bottom_nominal_moment_strenght, beam.nominal_shear_strenght)
