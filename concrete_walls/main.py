ecbdc062362862a446b188c81819d6bb8b0be5ac
unknown <nicolasmorenoy@gmail.com>
2022-11-09 20:01:42 -0500

    cambié el ancho

---
+++ concrete_walls/main.py
@@ -0,0 +1,16 @@
+#$ PIER/SPANDREL NAMES
+def main():
+    with open("./data/data.$et", "r") as f:
+        for line in f:
+            a = line
+            print(line,line)
+            break
+    
+    print(a == "$ File D:\Dropbox\53829\01_Torre 2\SuperEstructura_SanAgustin_T2_V00 - NM.$et saved 4/11/2022 9:54:03 a. m.  ")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bottom_rebar = Reinforcement(2,Rebar(5))
 stirrup_rebar = Reinforcement(2,TransverseRebar(3, 0.2))
 concrete = Concrete(28, EC_factor=3900)
 steel = Steel(420)
-geometry = Rectangular(0.3,0.4)
+geometry = Rectangular(0.4,0.4)
 beam = Beam(geometry, 5, 0.04, concrete,steel, top_rebar, bottom_rebar, stirrup_rebar)
 
 
