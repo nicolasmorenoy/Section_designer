c90f326d754841a24e82ce6594ed503bb68bbcd5
unknown <nicolasmorenoy@gmail.com>
2023-03-15 19:52:49 -0500

    Creation of section according to an ETABS rectangular cross section

--- beam_tables.py
+++ beam_tables.py
@@ -10,16 +10,5 @@ for b in sections_resume:
     n = sections_resume.index(b)
     b.set_section(Rectangular(float(sections['Width'][n]),float(sections['Depth'][n])))
 
-# sections_resume['Sections'] = sections[BeamSection
-# for row in sections:
-#     print(row)
-# sections["Section"] = Rectangular(sections["Width"], sections["Depth"]).cross_area
-# #print(sections)
-# # sections["Section"] = section_dict[sections["Name"]] = Rectangular(sections["Width"], sections["Depth"])
-# # for element in sections["Name"]:
-# #     section_dict[element] = 0
-# # print(section_dict)
-# #sections_resume= sections.loc[:,sections.columns == "Section"]
-# sections_resume= sections.loc[sections.columns == 'Section']
 for i in sections_resume:
     print(i.id,i.cross_section.lenght_1, i.cross_section.lenght_2)
